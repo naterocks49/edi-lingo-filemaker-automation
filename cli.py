8606b4354add5bd7c4e70b9fd16be5d05d0dbf05
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-20 14:27:59 -0400

    Updates

---
+++ cli.py
@@ -0,0 +1,27 @@
+from edi_api import EdiApi
+
+a = EdiApi()
+
+while 1 == 1:
+    request = input("Input function:")
+    print(f"Initiating: {request}.")
+
+
+    if request == "login":
+        a.login_to_api()
+        print(a.session_id)
+
+    elif request == "logout":
+        print(a.logout_of_api())
+
+    elif request == "today":
+        print(a.search_invoice_filter_by_today())
+
+    elif request == "getbyid":
+        id = input('Enter ID:')
+        print(a.get_invoice_by_id(id))
+
+    elif request == "partners":
+        print(a.check_partners())
+
+    elif request == "all_onhold":
